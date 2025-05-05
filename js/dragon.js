51bc7d05d15626cf43b457c2f70a77773645c757
Grant Pullen <gwpullen@iu.edu>
2025-05-05 13:59:22 -0400

    please

---
+++ js/dragon.js
@@ -0,0 +1,14 @@
+let Length = [];
+
+Length[Western] = 6;
+Length[Oriental] = 15;
+Length[Wyvern] = 8;
+Length[Hydra] = 8;
+
+console.log(scores[0]);
+console.log(scores[1]);
+console.log(scores[2]);
+
+let teams = ["Tigers", "Bisons", "Eagles", "Cobras"];
+
+console.log(teams);
